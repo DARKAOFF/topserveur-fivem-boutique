6cfeede7c60de9472d238f4ac7795e5fc9800904
Darka <anthony.perrot.92@gmail.com>
2026-04-30 02:05:37 +0200

    Add English locale

---
+++ locales/en.lua
@@ -0,0 +1,25 @@
+Locales = Locales or {}
+
+Locales.en = {
+  menu_title = 'Shop',
+  menu_help = 'Use ↑ ↓ to navigate, Enter to buy, Backspace to go back.',
+  open_hint = 'Open shop',
+  category = 'Category',
+  price = 'Price',
+  buy = 'Buy',
+  back = 'Back',
+  close = 'Close',
+  no_items = 'No item available in this category.',
+  purchase_confirm = 'Buy %s for %s %s?',
+  purchase_sent = 'Purchase sent to server.',
+  purchase_success = 'Purchase confirmed: %s.',
+  purchase_refused = 'Purchase refused: %s',
+  not_enough_points = 'You do not have enough points.',
+  vote_required = 'You must vote before using this shop.',
+  api_disabled = 'API disabled, local validation only.',
+  invalid_item = 'Item not found.',
+  player_not_found = 'Player not found.',
+  points_balance = 'Balance: %s %s',
+  loading = 'Loading...',
+  controls = '↑↓ Navigate  Enter Buy  Backspace Close'
+}
